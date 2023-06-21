7e1a8dd00cba5c58faaddfbd79251e7226cb07c8
Ahmar <ahmarkakimov2005@gmail.com>
2023-06-22 00:27:39 +0600

    Completed HomeWork1.1

---
+++ lesson 1/HW1/homeworkprojects/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <p class="name">Name: {{ user.name }}</p>
+    <p class="surname">Surname: {{ user.surname }}</p>
+    <p class="age">Age: {{ user.age }}</p>
+    <p class="mail">Email: {{ user.mail }}</p>
+    <p class="country">Country: {{ user.country }}</p>
+    <p class="id">User ID: {{ user.id }}</p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+let user = ref({
+  id: 1,
+  name: 'Ahmar',
+  surname: 'Kakimov',
+  age: 18,
+  mail: 'ahmarkakimov@gmail.com',
+  country: 'Kazakhstan'
+})
+</script>
+
+<style scoped>
+body {
+  background-color: gray;
+}
+</style>
