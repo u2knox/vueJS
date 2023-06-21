05c82080a64fa7410373b688f9916461dba02a98
Ahmar <ahmarkakimov2005@gmail.com>
2023-06-21 21:45:39 +0600

    first project

---
+++ firstProject/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <LikeCount />
+    <LikeCount />
+  </div>
+</template>
+
+<script setup>
+import LikeCount from '@/components/LikeCount.vue'
+</script>
+
+<style scoped></style>
---
+++ firstProject/src/components/LikeCount.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <span>Likes: {{ likes }} {{ Math.round((likes / (likes + dislikes)) * 100) || 0 }}%</span>
+    <span
+      >Dislikes: {{ dislikes }} {{ Math.round((dislikes / (likes + dislikes)) * 100) || 0 }}%</span
+    >
+    <button @click="addLike">like</button>
+    <button @click="addDislike">Dislike</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const likes = ref(0)
+const dislikes = ref(0)
+const addLike = () => likes.value++
+const addDislike = () => dislikes.value++
+</script>
+
+<style scoped></style>
