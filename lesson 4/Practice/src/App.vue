29d2ac0574805d5d1c9aecd6b15618eacc91e7e4
Ahmar <ahmarkakimov2005@gmail.com>
2023-07-04 21:53:57 +0600

    Completed Lesson 3

---
+++ lesson 4/Practice/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <user-task v-for="task in tasks" :key="task.id" :title="task.title" :content="task.content"></user-task>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+import UserTask from '@/components/UserTask.vue'
+
+const tasks = ref([
+  {
+    id:1,
+    title:'title1',
+    content: 'content1',
+  },
+  {
+    id:2,
+    title:'title2',
+    content: 'content2',
+  },
+  {
+    id:3,
+    title:'title3',
+    content: 'content3',
+  },
+  {
+    id:4,
+    title:'title4',
+    content: 'content4',
+  },
+  {
+    id:5,
+    title:'title5',
+    content: 'content5',
+  },
+])
+</script>
+
+<style scoped></style>
---
+++ lesson 4/vue-project/src/components/MyInput.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <label :for="`input${inputId}`">{{ props.label }}</label>
+    <input
+      :value="props.modelValue"
+      @input="$emit('update:modelValue', $event.target?.value)"
+      type="text"
+      :id="`input${inputId}`"
+      :placeholder="props.placeholder"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+
+interface Props {
+  label: string
+  placeholder?: string
+  modelValue: string
+}
+const props = defineProps<Props>()
+
+const inputId = Math.round(Math.random() * 10000000)
+</script>
+
+<style scoped>
+input {
+  background-color: cadetblue;
+  border-radius: 8px;
+  border: none;
+  width: 200px;
+  height: 25px;
+  font-size: medium;
+  color: white;
+}
+::-webkit-input-placeholder {
+  /* WebKit browsers */
+  color: rgb(0, 0, 0);
+}
+:-moz-placeholder {
+  /* Mozilla Firefox 4 to 18 */
+  color: rgb(0, 0, 0);
+}
+::-moz-placeholder {
+  /* Mozilla Firefox 19+ */
+  color: rgb(0, 0, 0);
+}
+:-ms-input-placeholder {
+  /* Internet Explorer 10+ */
+  color: rgb(0, 0, 0);
+}
+</style>
