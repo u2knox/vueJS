b6e974679fa04c90e4f40b13c6685c0e49cfda31
Ahmar <ahmarkakimov2005@gmail.com>
2023-07-11 01:42:59 +0600

    Completed HW5

--- lesson 5/HW5/src/App.vue
+++ lesson 5/HW5/src/App.vue
@@ -1,11 +1,59 @@
 <template>
   <div>
-
+    <slide-card
+      v-for="slide in content"
+      @prev="(slideId:any)=>prevSlide(slideId)"
+      @next="(slideId:any)=>nextSlide(slideId)"
+      v-show="slide.view"
+      :key="slide.id"
+      :slideId="slide.id"
+      :title="slide.title"
+      :body="slide.body"
+    ></slide-card>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
+import SlideCard from '@/components/SlideCard.vue'
+const content = ref([
+  {
+    id: 1,
+    view: true,
+    title: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+    body: 'quia et suscipitsuscipit recusandae consequuntur expedita et cumreprehenderit molestiae ut ut quas totamnostrum rerum est autem sunt rem eveniet architecto'
+  },
+  {
+    id: 2,
+    view: false,
+    title: 'qui est esse',
+    body: 'est rerum tempore vitaesequi sint nihil reprehenderit dolor beatae ea dolores nequefugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis qui aperiam non debitis possimus qui neque nisi nulla'
+  },
+  {
+    id: 3,
+    view: false,
+    title: 'ea molestias quasi exercitationem repellat qui ipsa sit aut',
+    body: 'et iusto sed quo iurevoluptatem occaecati omnis eligendi aut advoluptatem doloribus vel accusantium quis pariaturmolestiae porro eius odio et labore et velit aut'
+  }
+])
+function prevSlide(id:number) {
+  if (id != 1) {
+    content.value[id-2].view = true
+    content.value[id-1].view = false
+  }else{
+    content.value[content.value.length-1].view = true
+    content.value[id-1].view = false
+  }
+}
+function nextSlide(id:number){
+  if(id != content.value.length){
+    content.value[id].view = true
+    content.value[id-1].view = false
+  }else{
+    content.value[0].view = true
+    content.value[id-1].view = false
+  }
+}
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
--- lesson 5/HW5/src/components/SlideCard.vue
+++ lesson 5/HW5/src/components/SlideCard.vue
@@ -1,12 +1,56 @@
 <template>
+  <div class="card">
+    <div @click="emits(`prev`, props.slideId)" class="button" id="prev">⏩</div>
     <div>
-
+      <h3>{{ props.title }}</h3>
+      <h5>{{ props.body }}</h5>
     </div>
+    <div @click="emits('next', props.slideId)" class="button" id="next">⏩</div>
+  </div>
 </template>
 
 <script setup lang="ts">
+interface Props {
+  title: string
+  body: string
+  slideId: number
+}
+const props = defineProps<Props>()
+
+interface Emits{
+  (e:'next', id:number):void
+  (e:'prev', id:number):void
+}
+const emits = defineEmits<Emits>()
 </script>
 
 <style scoped>
-
+.card {
+  color: black;
+  border: 1px solid red;
+  border-radius: 15px;
+  width: 400px;
+  height: 200px;
+  background-color: turquoise;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  position: absolute;
+  top: 40%;
+  left: 40%;
+}
+.button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 15px;
+  width: 40px;
+  height: 40px;
+  background-color: red;
+  border-radius: 50px;
+  cursor: pointer;
+}
+#prev{
+    rotate: 180deg;
+}
 </style>
