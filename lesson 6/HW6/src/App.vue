842282d4ab172cc6c0e457f73b26818463c23bbc
Ahmar <ahmarkakimov2005@gmail.com>
2023-07-12 18:22:42 +0600

    Completed HW6

--- lesson 6/HW6/src/App.vue
+++ lesson 6/HW6/src/App.vue
@@ -1,47 +1,34 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <select id="random">
+      <option value="list">By the list</option>
+      <option value="random">Random</option>
+    </select>
+    <div class="block" v-random="materials"></div>
+  </div>
 </template>
 
+<script setup lang="ts">
+import {ref} from 'vue'
+const materials = ref({
+  img: [
+    'https://blog.emandarine.net/grille/templates/all-css/imgTest/2.jpg',
+    'https://images.pexels.com/photos/1592384/pexels-photo-1592384.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    'https://upload.wikimedia.org/wikipedia/commons/0/00/500x500.jpg',
+    'https://2.bp.blogspot.com/-CmBgofK7QzU/TVj3u3N1h2I/AAAAAAAADN8/OszBhGvvXRU/s640/tumblr_lg7h9gpbtP1qap9qio1_500.jpeg'
+  ],
+  timeout: 3000
+})
+ console.log(materials.value)
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
+.block{
+  width: 500px;
+  height: 500px;
+  background-color: red;
+  border: 1px solid black;
+  background-image: url('https://lespetitespensees.com/wp-content/uploads/2013.bak/10/500x500.gif');
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
--- lesson 6/HW6/src/main.ts
+++ lesson 6/HW6/src/main.ts
@@ -3,4 +3,24 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App)
+  .directive('random', {
+    mounted(el, bind) {
+      let img = 0
+      setInterval(() => {
+        const select = document.querySelector('#random').value
+        if (select == 'random') {
+          let random = Math.floor(Math.random()*4)
+          el.style.backgroundImage = `url('${bind.value.img[random]}')`
+        } else {
+          el.style.backgroundImage = `url('${bind.value.img[img]}')`
+          img++
+          if (img == bind.value.img.length) {
+            img = 0
+            console.log(img)
+          }
+        }
+      }, bind.value.timeout)
+    }
+  })
+  .mount('#app')
